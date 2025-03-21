a2ae4c1048286c4a5d3f4f889dc157f056910a18
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-21 17:58:23 +0000

    Update build_win.bat

--- build_win.bat
+++ build_win.bat
@@ -4,11 +4,81 @@ setlocal EnableDelayedExpansion
 REM Download latest libplacebo
 git clone --recursive https://github.com/haasn/libplacebo
 
-mkdir libplacebo\build
+mkdir libplacebo\include
 
-DIR=libplacebo\build
-cd libplacebo
-meson $DIR
-$ meson configure $DIR
+xcopy /y /v /s /e libplacebo\src\include libplacebo\include
+
+dir libplacebo\include\libplacebo
+
+exit /b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mkdir upload
+mkdir upload\include\libxml2
+mkdir upload\Debug
+mkdir upload\Release
+
+REM Download latest libXML2
+git clone https://github.com/GNOME/libxml2
+
+REM Build Debug configuration
+cmake -S libxml2 -B build -D LIBXML2_WITH_ICONV=OFF -D LIBXML2_WITH_PYTHON=OFF -D CMAKE_BUILD_TYPE=Debug
+cmake --build build --config Debug
+cmake --install build
+
+REM Copy over the built files
+copy /y /v build\Debug\*.dll upload\Debug
+copy /y /v build\Debug\*.lib upload\Debug
+copy /y /v build\Debug\libxml2d.pdb upload\Debug
+
+REM Clean up before we run CMake again
+rmdir /s /q build
+
+REM Build Release configuration
+cmake -S libxml2 -B build -D LIBXML2_WITH_ICONV=OFF -D LIBXML2_WITH_PYTHON=OFF -D CMAKE_BUILD_TYPE=Release
+cmake --build build --config Release
+cmake --install build
+
+REM Copy over the built files
+copy /y /v build\Release\*.dll upload\Release
+copy /y /v build\Release\*.lib upload\Release
+
+REM Copy over the headers
+xcopy /y /v /s /e libxml2\include upload\include\libxml2
 
 exit /b
